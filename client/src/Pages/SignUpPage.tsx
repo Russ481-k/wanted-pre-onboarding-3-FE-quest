a123905776696e3cf51f6595db853f16958325ed
Russ <yunsubin481@gmail.com>
2023-02-25 17:33:52 +0900

    SignUpPage

---
+++ client/src/Pages/SignUpPage.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import Footer from "../Components/Footer";
+import AssetWrapper from "../Wrapper/AssetWrapper";
+import HistoryWrapper from "../Wrapper/HistoryWrapper";
+
+const SignUpPage =() => {
+
+    return(
+    <div className="sign_up_page">
+        <div className="sign_up_page_head">
+            <h1>Sign Up</h1>
+        </div>
+        <div className="sign_up_page_body">
+            <div className="sign_up_page_body_input">
+                <h1>ID</h1>
+                <input type="text" placeholder="ID"></input>
+            </div>
+            <div className="sign_up_page_body_input">
+                <h1>PW</h1>
+                <input type="password" placeholder="PW"></input>
+            </div>
+            <div className="sign_up_page_body_input">
+                <h5>PW Check</h5>
+                <input type="password" placeholder="PW"></input>
+            </div>
+
+        </div>
+        <div className="sign_up_page_footer">
+            <button><h3>Sign Up</h3></button>
+            <Link to='/log_in'><h3>LogIn</h3></Link>
+    </div>    </div>
+    )
+}
+export default SignUpPage;
