72905c7bf60cca713c628bc78ec91ea98eab3af8
Russ <yunsubin481@gmail.com>
2023-02-25 17:33:04 +0900

    Header

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
 
 const Header =() => {
 
     return(
     <div className="header">
-        <h1>Header</h1>
+        <Link to = "/"><img src={require('../images/logo.png')} alt='logo'/></Link>
+        
+        <Link to = "/"><img className="main_name" src={require('../images/ENTASIS.png')} alt='entasis' /></Link>
+
+        <div className="user_state">
+            <Link to = "/log_in"><h3>Log In</h3></Link>
+            <Link to = "/sign_up"><h3>Sign Up</h3></Link>
+            <Link to = "/"><img src={require('../images/none_user.png')} alt='user' width="40px"/></Link>
+        </div>
     </div>
     )
 }
